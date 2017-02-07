3a1d72ddca2cf38d1d864ce3c7e594c59feb84a5
Adhil Roshan N A <adhilr96@gmail.com>
2017-02-08 00:13:02 +0530

    Update 19.c

--- 19.c
+++ 19.c
@@ -3,6 +3,7 @@
 
 void swap(char a[],char b[])
 {
+	//sort
 	char t[10];
 	strcpy(t,a);
 	strcpy(a,b);
@@ -12,6 +13,7 @@ void swap(char a[],char b[])
 void sort(char a[][10],int n)
 {
 	int i,j,k,alen,blen;
+	//linear sort
 	for(i=0;i<n-1;i++)
 	{
 		for(j=i+1;j<n;j++)
@@ -20,19 +22,23 @@ void sort(char a[][10],int n)
 			blen=strlen(a[j]);
 			if(blen<alen)
 			{
+				//if the length of the string(number of digits) is greater,then it is the greater number
 				swap(a[i],a[j]);
 			}
 			else if(alen==blen)
 			{
+				//if number of digits are equal check for the greater no. amongst them
 				for(k=0;k<alen;k++)
 				{
 					if(a[i][k]>a[j][k])
 					{
+						//if the higher significant didgit is greater, then it is the greater number
 						swap(a[i],a[j]);
 						break;
 					}
 					else if(a[i][k]>a[j][k])
 					{
+						//if the 2nd number is greater go to the next iteration
 						break;
 					}
 				}
